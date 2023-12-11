dd942988b1dce86ceb6f2329e48c820dd0f0e686
ManasRaj241 <manasranjansatapathy96@gmail.com>
2023-12-11 19:48:47 +0530

    FrontEnd And Backend Setup

---
+++ FrontEnd/ui/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-white mt-10">
+        <h1>Newly Added Code</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
